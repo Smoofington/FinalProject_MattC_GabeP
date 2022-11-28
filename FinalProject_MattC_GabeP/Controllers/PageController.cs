a103259b98f965f1be6ca58129719fdd14d4c464
Gabriel Phan <phanga@mail.uc.edu>
2022-11-28 12:13:31 -0500

    Controller for pages and Pages needed

---
+++ FinalProject_MattC_GabeP/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject_MattC_GabeP.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
