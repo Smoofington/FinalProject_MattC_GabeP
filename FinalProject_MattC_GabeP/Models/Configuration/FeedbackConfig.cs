d5d084a3eac59940c6e2b6bcc53fbe4170438ba3
Matthew Caudill <82420942+Smoofington@users.noreply.github.com>
2022-12-02 17:33:36 -0600

    Update FeedbackConfig.cs

--- FinalProject_MattC_GabeP/Models/Configuration/FeedbackConfig.cs
+++ FinalProject_MattC_GabeP/Models/Configuration/FeedbackConfig.cs
@@ -9,8 +9,8 @@ namespace FinalProject_MattC_GabeP.Models.Configuration
         public void Configure(EntityTypeBuilder<FeedbackViewModel> entity)
         {
             entity.HasData(
-                new FeedbackViewModel { FeedbackId = 1, FeedbackTitle = "Matt", FeedbackDescription = "Smells great,tastes great, all things people say about websites." },
-                new FeedbackViewModel { FeedbackId = 2, FeedbackTitle = "Gabe", FeedbackDescription = "Well, we created this so........ it looks good to me." }
+                new FeedbackViewModel { FeedbackId = 1, FeedbackName = "Matt", FeedbackText = "Smells great,tastes great, all things people say about websites." },
+                new FeedbackViewModel { FeedbackId = 2, FeedbackName = "Gabe", FeedbackText = "Well, we created this so........ it looks good to me." }
                 );
 
             
