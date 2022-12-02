10c5c3a128df3dc212ee4dce50e07a32d389b974
Smoofington <caudill.matthew0127@gmail.com>
2022-12-02 16:12:33 -0600

    matt stuff

---
+++ FinalProject_MattC_GabeP/Models/DomainModels/Feedback.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalProject_MattC_GabeP.Models.DomainModels
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }        // pk
+        
+
+        public string FeedbackTitle { get; set; }
+    }
+}
