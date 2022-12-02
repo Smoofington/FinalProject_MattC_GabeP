ed461b9684d95b7f6bb141708b40f1e5d2aaa59b
Gabriel Phan <phanga@mail.uc.edu>
2022-12-02 17:54:45 -0500

    Feedback DB display and view stuff for index/feedback pages

--- FinalProject_MattC_GabeP/Controllers/HomeController.cs
+++ FinalProject_MattC_GabeP/Controllers/HomeController.cs
@@ -13,9 +13,12 @@ namespace FinalProject_MattC_GabeP.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private MattC_GabePContext context { get; }
+
+        public HomeController(ILogger<HomeController> logger, MattC_GabePContext context)
         {
             _logger = logger;
+            this.context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +35,21 @@ namespace FinalProject_MattC_GabeP.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View("Feedback", new FeedbackViewModel() { FeedbackName = "", FeedbackText = ""});
+        }
+
+        [HttpPost]
+        public IActionResult Saving(FeedbackViewModel feedback)
+        {
+
+            context.Feedback.Add(feedback);
+            context.SaveChanges();
+            return View("Index");
         }
     }
 }
--- FinalProject_MattC_GabeP/Models/FeedbackViewModel.cs
+++ FinalProject_MattC_GabeP/Models/FeedbackViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace FinalProject_MattC_GabeP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject_MattC_GabeP.Models
 {
     public class FeedbackViewModel
     {
+        [Key]
+        public int FeedbackId { get; set; }
         public string FeedbackName { get; set; }
         public string FeedbackText { get; set; }
     }
