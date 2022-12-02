56eae33197a89a0834b92ba6c03748ebfd4f7c57
Gabriel Phan <phanga@mail.uc.edu>
2022-12-01 23:34:40 -0500

    Adding content

--- FinalProject_MattC_GabeP/Controllers/PageController.cs
+++ FinalProject_MattC_GabeP/Controllers/PageController.cs
@@ -4,9 +4,25 @@ namespace FinalProject_MattC_GabeP.Controllers
 {
     public class PageController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Gabe()
         {
             return View();
         }
+
+        public IActionResult Matt()
+        {
+            return View();
+        }
+
+        public IActionResult GabeHobby()
+        {
+            return View();
+        }
+
+        public IActionResult MattHobby()
+        {
+            return View();
+        }
+
     }
 }
--- FinalProject_MattC_GabeP/Program.cs
+++ FinalProject_MattC_GabeP/Program.cs
@@ -13,7 +13,9 @@ namespace FinalProject_MattC_GabeP
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
